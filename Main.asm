6d824a29f12c16d6d69453a731993002a3122241
Taynara Araújo <112909056+Thay483@users.noreply.github.com>
2022-11-29 15:08:22 -0300

    Impressão Matriz (Funcionando)

--- Main.asm
+++ Main.asm
@@ -1,10 +1,45 @@
 .MODEL SMALL
+
+pula_linha MACRO proc
+mov ah,02h
+mov dl,10
+int 21h    
+endm
+
 .data
+MATRIZ db 9 dup (31h,32h,33h,34h,35h,36h,37h,38h,39h)
 
 .stack 100h
 .code
 
 main proc
+
+mov ax,@data
+mov ds,ax
+lea bx,MATRIZ ;o vetor armazenado em bx
+
+mov ah,02h
+mov ch,9 ;contador para linha
+
+inicio:
+mov cl,9 ;contador para coluna
+xor si,si ;será a linha
+
+mover:
+mov dl, [bx][si]
+int 21h
+inc si ;exibindo a linha primeira linha da matriz
+dec cl ;vai pulando de colunas 9 vezes
+jnz mover
+
+pula_linha
+
+add bx, 9 ;contador de coluna
+dec ch ;decrementa linha
+jnz inicio
+
+mov ah,4ch
+int 21h
 
 main endp
 
