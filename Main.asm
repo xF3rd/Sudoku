f3664b68a0cc1185af24d02db787b319919be369
xF3rd <fernando.bordin.correa@gmail.com>
2022-11-29 18:37:55 -0300

    comecei a fazer o troca numero

--- Main.asm
+++ Main.asm
@@ -1,12 +1,12 @@
 .MODEL SMALL
 
-pula_linha MACRO proc
+pula_linha MACRO proc               ;macro de pular linha
 mov ah,02h
 mov dl,10
 int 21h    
 endm
 
-linha_vert macro proc
+linha_vert macro proc               ;macro para fazer linha na vertical
 mov ah,02h
 mov dl,' '
 int 21h
@@ -16,21 +16,21 @@ mov dl,' '
 int 21h
 endm
 
-push_reg macro proc
+push_reg macro proc                 ;macro de push de registradores
 push ax
 push bx
 push cx
 push dx
 endm
 
-pop_reg macro proc
+pop_reg macro proc                  ;macro de pop de registradores
 pop ax
 pop bx
 pop cx
 pop dx
 endm
 
-barra macro proc
+barra macro proc                    ;faz barra na horizontal
 mov cl,35
 
 loop1:
@@ -49,6 +49,9 @@ endm
 
 .data
 MATRIZ db 9 dup (31h,32h,33h,34h,35h,36h,37h,38h,39h)
+msg1 db 'Qual coluna quer mudar:$'
+msg2 db 'Qual linha quer mudar:$'
+msg3 db 'Qual numero vai colocar:$'
 
 .stack 100h
 .code
@@ -56,11 +59,13 @@ MATRIZ db 9 dup (31h,32h,33h,34h,35h,36h,37h,38h,39h)
 main proc
 
     mov ax,@data
-    mov ds,ax
-    lea bx,MATRIZ ;o vetor armazenado em bx
+    mov ds,ax                   ;inicializa ds
+    mov es,ax                   ;inicializa es
+    lea bx,MATRIZ               ;o vetor armazenado em bx
 
-    call imprime_matriz
+    
     call troca_numero
+    call imprime_matriz
 
     mov ah,4ch
     int 21h
@@ -69,38 +74,63 @@ main endp
 
 imprime_matriz proc
 
+    xor bx,bx
+    xor si,si               ;limpa reg
+
     mov ah,02h
-    mov ch,9 ;contador para linha
+    mov ch,9                ;contador para linha
 
     inicio:
     barra
-    mov cl,9 ;contador para coluna
-    xor si,si ;será a linha
+    mov cl,9                ;contador para coluna
+    xor si,si               ;será a linha
 
     mover:
-    mov dl, [bx][si]
+    mov dl,MATRIZ[bx][si]
     int 21h
     linha_vert
-    inc si ;exibindo a linha primeira linha da matriz
-    dec cl ;vai pulando de colunas 9 vezes
+    inc si                  ;exibindo a linha primeira linha da matriz
+    dec cl                  ;vai pulando de colunas 9 vezes
     jnz mover
 
     pula_linha
 
-    add bx, 9 ;contador de coluna
-    dec ch ;decrementa linha
+    add bx, 9               ;contador de coluna
+    dec ch                  ;decrementa linha
     jnz inicio
 
     ret
 imprime_matriz endp
 
 troca_numero proc
+
+    xor si,si
+    xor bx,bx
+
+    mov dl,offset msg1
+    mov ah,09
+    int 21h
+    mov ah,01
+    int 21h
+    sub al,30h
+    mov bx,ax
+
+    mov dl,offset msg2
+    mov ah,09
+    int 21h
+    mov ah,01
+    int 21h
+    sub al,30h
+    mov si,ax
+
+    mov dl,offset msg3
+    mov ah,09
+    int 21h
+    mov ah,01
+    int 21h
+    mov matriz[bx][si],al
+
     
-    mov bx,bx ;coluna 8
-    xor si,si ;linha 0
-
-    mov MATRIZ[bx][si],'?'
-
     ret
 troca_numero endp
 
